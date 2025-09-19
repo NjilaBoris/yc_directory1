365f49563e75288ccc2c2a9e18becee98c8e8d16
NjilaBoris <njilaboris2@gmail.com>
2025-09-20 00:30:54 +0100

    Implement View Query

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -7,7 +7,7 @@ import { notFound } from "next/navigation";
 import React, { Suspense } from "react";
 import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
-import { View } from "lucide-react";
+import View from "@/components/View";
 
 const md = markdownit();
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
--- components/View.tsx
+++ components/View.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Ping from "./Ping";
 
-const View = () => {
-  return <div>View</div>;
+const View = ({ id }: { id: string }) => {
+  return (
+    <div className="view-container">
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
+
+      <p className="view-text">
+        <span className="font-black">Views: {totalViews}</span>
+      </p>
+    </div>
+  );
 };
 
 export default View;
