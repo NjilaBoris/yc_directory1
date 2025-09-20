f6373086ce29c310a916dcb7890f380776d632a6
NjilaBoris <njilaboris2@gmail.com>
2025-09-20 01:00:07 +0100

    Implement View Component Final

--- components/View.tsx
+++ components/View.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import Ping from "./Ping";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
+import { after } from "next/server";
+import { writeClient } from "@/sanity/lib/write-client";
 
-const View = ({ id }: { id: string }) => {
+const View = async ({ id }: { id: string }) => {
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
