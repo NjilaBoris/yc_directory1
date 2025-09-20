e3e363353a86a4baa3810676bbb1a72f78953079
NjilaBoris <njilaboris2@gmail.com>
2025-09-20 14:25:57 +0100

    Implement StartupsByAuthor Query

--- components/UserStartups.tsx
+++ components/UserStartups.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import StartupCard, { StartupTypeCard } from "./StartupCard";
 import { client } from "@/sanity/lib/client";
+import { STARTUPS_BY_AUTHOR_QUERY } from "@/sanity/lib/queries";
 
 const UserStartups = async ({ id }: { id: string }) => {
   const startups = await client.fetch(STARTUPS_BY_AUTHOR_QUERY, { id });
